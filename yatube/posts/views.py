433f64e77684a83156eff442421e52f938dd5956
damir <d4mirshd@yandex.ru>
2022-03-10 17:06:59 +0300

    sprint_6_done

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -128,11 +128,10 @@ def follow_index(request):
 @login_required
 def profile_follow(request, username):
     author = get_object_or_404(User, username=username)
-    if author != request.user:
-        if not Follow.objects.filter(
-                user=request.user, author=author
-        ).exists():
-            Follow.objects.create(user=request.user, author=author)
+    if author != request.user and not Follow.objects.filter(
+        user=request.user, author=author
+    ).exists():
+        Follow.objects.create(user=request.user, author=author)
     return redirect("posts:follow_index")
 
 
